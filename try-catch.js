ccce289db2a87012ef69a84c668a5a7a7d7a5d27
masum691 <masumhu691@gmail.com>
2021-08-20 15:49:06 +0600

    try catch

---
+++ try-catch.js
@@ -0,0 +1,38 @@
+
+
+
+const name = 'Alpina';
+try{
+    name = 'Akter';
+}
+catch(error){
+    console.log('error is', error)
+}
+console.log(122);
+console.log(name);
+
+
+// const name = 'Ali';
+// try{
+//     name = 'Mia';
+// }
+// catch(error){
+//     console.log(error);
+// }
+// console.log(name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
