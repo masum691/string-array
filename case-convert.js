0ef30366f208a369d3d5072d07b84177ea75b934
masum691 <masumhu691@gmail.com>
2021-08-20 10:02:31 +0600

    done case convert uppercase and lowercase

--- case-convert.js
+++ case-convert.js
@@ -1,2 +1,34 @@
 
-const anthem = 'I Love Allah And Prophet Muhammed Sm';
+// const anthem = 'I Love Allah And Prophet Muhammed Sm';
+// const search = 'Allah';
+
+// const userInput = 'mymensingH';
+// const savedUserName = 'Mymensingh';
+// if(userInput == savedUserName){
+//     console.log('real user');
+// }
+// else{
+//     console.log('Its not real user');
+// }
+
+// toLowerCase()
+const firstUserInput = 'mymensingH';
+const firstSavedUserName = 'Mymensingh';
+if(firstUserInput.toLowerCase() == firstSavedUserName.toLowerCase()){
+    console.log('real user');
+}
+else{
+    console.log('Its not real user');
+}
+
+// toLowerCase()
+const userInput = 'mymensingH';
+const savedUserName = 'Mymensingh';
+// console.log(userInput.toUpperCase());
+if(userInput.toUpperCase() == savedUserName.toUpperCase()){
+    console.log('real user');
+    
+}
+else{
+    console.log('Its not real user');
+}
