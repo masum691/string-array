1149193e0c21c54b004c2c395d015f6086084230
masum691 <masumhu691@gmail.com>
2021-08-20 16:02:52 +0600

    date js

---
+++ date.js
@@ -0,0 +1,17 @@
+
+const myFavDate = new Date('1971-12-16');
+console.log(myFavDate);
+
+const anotherDate = new Date(1971, 3, 26, 11, 5, 40, 79);
+console.log(anotherDate);
+
+
+if(myFavDate < anotherDate){
+    console.log('Fav')
+}
+
+
+// moment js
+
+
+// javascript compare to dates - search it on google
